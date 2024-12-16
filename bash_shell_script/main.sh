f355a80146b6b381039f4c60bc7fd3dce62482c9
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2024-12-16 14:50:41 +0530

    Some Major changes to the bash script

--- bash_shell_script/main.sh
+++ bash_shell_script/main.sh
@@ -3,12 +3,39 @@
 BOARD_NAME=""
 FQBN_SELECTED=""
 
+editors=("nano" "micro" "vim" "nvim" "hx" "code" "codium")
+installed_editors=()
+
+sketch_file=""
+
+edit_sketch() {
+  echo "Choose file to edit : "
+  sketch_file=$(gum file --height 6)
+  sleep 0.5
+  echo "Choose your preferred editor."
+  echo "Note that these editors are already installed in your system"
+
+  sleep 1
+
+  for editor in "${editors[@]}"; do
+    if command -v $editor &> /dev/null; then
+      installed_editors+=("$editor")
+    fi
+  done
+  
+  chosen_editor=$(printf "%s\n" "${installed_editors[@]}" | gum choose)
+
+  $chosen_editor $sketch_file
+
+  main
+
+}
 
 create_new_sketch() {
   arduino-cli sketch new $1
   echo "New Sketch Created"
   echo "At path ${PWD}" | gum style --foreground 47
-  
+
   sleep 2
   clear
   main
@@ -16,9 +43,9 @@ create_new_sketch() {
 
 upload_code() {
   echo "Select file to be uploaded" 
-  local file_chosen_u=$(gum file --height 5)
-  echo "Uploaded Sketch" $file_chosen_u
-  gum style --foreground 47 $file_chosen_u 
+  #local file_u=$(gum file --height 5)
+  echo "Uploaded Sketch" $sketch_file
+  gum style --foreground 47 $sketch_file
   
   sleep 2
   clear
@@ -27,13 +54,12 @@ upload_code() {
 
 compile_code() {
   echo "Select file to be compiled"
-  local file_chosen_c=$(gum file --height 5)
+  #local file_c=$(gum file --height 5)
+  arduino-cli compile --fqbn $FQBN_SELECTED $sketch_file -v
   echo "Compiled Sketch at path" 
-  gum style --foreground 47 $file_chosen_c 
+  gum style --foreground 47 $sketch_file 
 
-  sleep 2
-  clear
-  main
+  gum confirm "Return to Home?" && main || exit
 }
 
 
@@ -66,23 +92,28 @@ main() {
   local intro="Welcome to arduino-cli-interactive"
   echo "$intro" | gum style --foreground 47 --border-foreground 217 --border double --align center --width 50
   echo "You have chosen the board : $BOARD_NAME 
-        with FQBN : $FQBN_SELECTED"
+        with FQBN : $FQBN_SELECTED
+        Sketch file :  $sketch_file"
 
   sleep 1
-  local choice=$(gum choose "Create New Sketch" "Compile Code" "Upload Code" "List Boards" "Exit")
+  local choice=$(gum choose "Create New Sketch" "Edit the Sketch" "Compile Code" "Upload Code" "Select Board" "Exit")
 
   case $choice in 
     "Create New Sketch")
       local sketch_name=$(gum input --placeholder "Enter Name of Sketch")
       create_new_sketch $sketch_name
       ;;
+    "Edit the Sketch")
+      edit_sketch
+      ;;
+      
     "Compile Code")
       compile_code
       ;;
     "Upload Code")
       upload_code
       ;;
-    "List Boards")
+    "Select Board")
       list_installed_boards
       ;;
     "Exit")
