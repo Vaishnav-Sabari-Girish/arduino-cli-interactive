af2353d721eccd62a4da913aa9e668ee4d64084c
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-01-24 20:09:09 +0530

    feat(portability): This new release has made it more portable
    Made it more portable to be able to run in macOS

--- bash_shell_script/main.sh
+++ bash_shell_script/main.sh
@@ -21,7 +21,7 @@ serial_monitor() {
 }
 
 check_for_updates() {
-  local current_version="v1.0.6"
+  local current_version="v1.0.7"
   
   # First check if GitHub token is set
   if [ -z "$ACI_GITHUB_TOKEN" ]; then
